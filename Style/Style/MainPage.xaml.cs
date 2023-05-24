b522b7439b97dbbb9bb557787ad8c7978299ac9d
Krzysiek <krzysiekali@zstjaslo.pl>
2023-05-24 11:21:21 +0200

    Dodaj pliki projektów.

---
+++ Style/Style/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Style
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(apka.Style == (Xamarin.Forms.Style)Resources["jasny"])
+            {
+                apka.Style = (Xamarin.Forms.Style)Resources["ciemny"];
+            }else apka.Style = (Xamarin.Forms.Style)Resources["jasny"];
+
+
+        }
+    }
+}
